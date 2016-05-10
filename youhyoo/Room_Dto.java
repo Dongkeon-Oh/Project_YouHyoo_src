b68a7e168899dbd4ee34fc1fc548fcf90f18f3ff
JeongEun <ojdms207@naver.com>
2016-05-10 16:03:03 +0900

    version 0.0.4.L

---
+++ youhyoo/Room_Dto.java
@@ -0,0 +1,82 @@
+package youhyoo;
+
+
+public class Room_Dto {
+	private int r_rum;
+	private String r_name;
+	private int r_maxpri_wd; 
+	private int r_minpri_we;
+	private int r_maxpri_we;
+	private int r_minpri_wd; 
+	private int r_maxcapa; 
+	private int r_mincapa; 
+	private int r_size; 
+	private String r_photo; 
+	private int r_pension;
+	public int getR_rum() {
+		return r_rum;
+	}
+	public void setR_rum(int r_rum) {
+		this.r_rum = r_rum;
+	}
+	public String getR_name() {
+		return r_name;
+	}
+	public void setR_name(String r_name) {
+		this.r_name = r_name;
+	}
+	public int getR_maxpri_wd() {
+		return r_maxpri_wd;
+	}
+	public void setR_maxpri_wd(int r_maxpri_wd) {
+		this.r_maxpri_wd = r_maxpri_wd;
+	}
+	public int getR_minpri_we() {
+		return r_minpri_we;
+	}
+	public void setR_minpri_we(int r_minpri_we) {
+		this.r_minpri_we = r_minpri_we;
+	}
+	public int getR_maxpri_we() {
+		return r_maxpri_we;
+	}
+	public void setR_maxpri_we(int r_maxpri_we) {
+		this.r_maxpri_we = r_maxpri_we;
+	}
+	public int getR_minpri_wd() {
+		return r_minpri_wd;
+	}
+	public void setR_minpri_wd(int r_minpri_wd) {
+		this.r_minpri_wd = r_minpri_wd;
+	}
+	public int getR_maxcapa() {
+		return r_maxcapa;
+	}
+	public void setR_maxcapa(int r_maxcapa) {
+		this.r_maxcapa = r_maxcapa;
+	}
+	public int getR_mincapa() {
+		return r_mincapa;
+	}
+	public void setR_mincapa(int r_mincapa) {
+		this.r_mincapa = r_mincapa;
+	}
+	public int getR_size() {
+		return r_size;
+	}
+	public void setR_size(int r_size) {
+		this.r_size = r_size;
+	}
+	public String getR_photo() {
+		return r_photo;
+	}
+	public void setR_photo(String r_photo) {
+		this.r_photo = r_photo;
+	}
+	public int getR_pension() {
+		return r_pension;
+	}
+	public void setR_pension(int r_pension) {
+		this.r_pension = r_pension;
+	} 
+}//class
---
+++ youhyoo/User_Dto.java
@@ -0,0 +1,99 @@
+package youhyoo;
+
+
+public class User_Dto {
+
+	private String u_id;
+	private String u_pwd;
+	private String u_type;
+	private String u_cell;
+	private String u_addr1;
+	private String u_addr2;
+	private String u_addr3;
+	private String u_addr4;
+	private String u_addr5;
+	private String u_zipcode;
+	private int birth;
+	private String u_email;
+	private int u_point;
+	
+	public String getU_id() {
+		return u_id;
+	}
+	public void setU_id(String u_id) {
+		this.u_id = u_id;
+	}
+	public String getU_pwd() {
+		return u_pwd;
+	}
+	public void setU_pwd(String u_pwd) {
+		this.u_pwd = u_pwd;
+	}
+	public String getU_type() {
+		return u_type;
+	}
+	public void setU_type(String u_type) {
+		this.u_type = u_type;
+	}
+	public String getU_cell() {
+		return u_cell;
+	}
+	public void setU_cell(String u_cell) {
+		this.u_cell = u_cell;
+	}
+	public String getU_addr1() {
+		return u_addr1;
+	}
+	public void setU_addr1(String u_addr1) {
+		this.u_addr1 = u_addr1;
+	}
+	public String getU_addr2() {
+		return u_addr2;
+	}
+	public void setU_addr2(String u_addr2) {
+		this.u_addr2 = u_addr2;
+	}
+	public String getU_addr3() {
+		return u_addr3;
+	}
+	public void setU_addr3(String u_addr3) {
+		this.u_addr3 = u_addr3;
+	}
+	public String getU_addr4() {
+		return u_addr4;
+	}
+	public void setU_addr4(String u_addr4) {
+		this.u_addr4 = u_addr4;
+	}
+	public String getU_addr5() {
+		return u_addr5;
+	}
+	public void setU_addr5(String u_addr5) {
+		this.u_addr5 = u_addr5;
+	}
+	public String getU_zipcode() {
+		return u_zipcode;
+	}
+	public void setU_zipcode(String u_zipcode) {
+		this.u_zipcode = u_zipcode;
+	}
+	public int getBirth() {
+		return birth;
+	}
+	public void setBirth(int birth) {
+		this.birth = birth;
+	}
+	public String getU_email() {
+		return u_email;
+	}
+	public void setU_email(String u_email) {
+		this.u_email = u_email;
+	}
+	public int getU_point() {
+		return u_point;
+	}
+	public void setU_point(int u_point) {
+		this.u_point = u_point;
+	}
+}//class
+
