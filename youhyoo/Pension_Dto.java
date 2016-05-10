f10e874b121f26b3c570fab466d637da215c535f
Jinkyeong Han <wlsrud3390@naver.com>
2016-05-10 15:50:37 +0900

    version 0.0.3.H

---
+++ youhyoo/Pension_Dto.java
@@ -0,0 +1,119 @@
+package youhyoo;
+
+public class Pension_Dto {
+	private int p_num;
+	private String p_name;
+	private String p_addr1;
+	private String p_addr2;
+	private String p_addr3;
+	private String p_addr4;
+	private String p_addr5;
+	private String zipcode;
+	private String tel;
+	private int paytype;
+	private String account;
+	private String intro;
+	private String photo;
+	private String contect;
+	private double lat;
+	private double lng;
+	
+	public Pension_Dto(){}
+	
+	public int getP_num() {
+		return p_num;
+	}
+	public void setP_num(int p_num) {
+		this.p_num = p_num;
+	}
+	public String getP_name() {
+		return p_name;
+	}
+	public void setP_name(String p_name) {
+		this.p_name = p_name;
+	}
+	public String getP_addr1() {
+		return p_addr1;
+	}
+	public void setP_addr1(String p_addr1) {
+		this.p_addr1 = p_addr1;
+	}
+	public String getP_addr2() {
+		return p_addr2;
+	}
+	public void setP_addr2(String p_addr2) {
+		this.p_addr2 = p_addr2;
+	}
+	public String getP_addr3() {
+		return p_addr3;
+	}
+	public void setP_addr3(String p_addr3) {
+		this.p_addr3 = p_addr3;
+	}
+	public String getP_addr4() {
+		return p_addr4;
+	}
+	public void setP_addr4(String p_addr4) {
+		this.p_addr4 = p_addr4;
+	}
+	public String getP_addr5() {
+		return p_addr5;
+	}
+	public void setP_addr5(String p_addr5) {
+		this.p_addr5 = p_addr5;
+	}
+	public String getZipcode() {
+		return zipcode;
+	}
+	public void setZipcode(String zipcode) {
+		this.zipcode = zipcode;
+	}
+	public String getTel() {
+		return tel;
+	}
+	public void setTel(String tel) {
+		this.tel = tel;
+	}
+	public int getPaytype() {
+		return paytype;
+	}
+	public void setPaytype(int paytype) {
+		this.paytype = paytype;
+	}
+	public String getAccount() {
+		return account;
+	}
+	public void setAccount(String account) {
+		this.account = account;
+	}
+	public String getIntro() {
+		return intro;
+	}
+	public void setIntro(String intro) {
+		this.intro = intro;
+	}
+	public String getPhoto() {
+		return photo;
+	}
+	public void setPhoto(String photo) {
+		this.photo = photo;
+	}
+	public String getContect() {
+		return contect;
+	}
+	public void setContect(String contect) {
+		this.contect = contect;
+	}
+	public double getLat() {
+		return lat;
+	}
+	public void setLat(double lat) {
+		this.lat = lat;
+	}
+	public double getLng() {
+		return lng;
+	}
+	public void setLng(double lng) {
+		this.lng = lng;
+	}
+}
