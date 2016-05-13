80ba8c587e7dece3072baf401963f4be433f7444
Jinkyeong Han <wlsrud3390@naver.com>
2016-05-13 10:06:49 +0900

    v 0.0.14.H Dto,Dao 수정

--- youhyoo/User_Dto.java
+++ youhyoo/User_Dto.java
@@ -1,19 +1,15 @@
 package youhyoo;
 
-
 public class User_Dto {
 
 	private String u_id;
+	private String u_name;	
 	private String u_pwd;
 	private String u_type;
 	private String u_cell;
-	private String u_addr1;
-	private String u_addr2;
-	private String u_addr3;
-	private String u_addr4;
-	private String u_addr5;
+	private String u_addr;
 	private String u_zipcode;
-	private int birth;
+	private int u_birth;
 	private String u_email;
 	private int u_point;
 	
@@ -22,6 +18,12 @@ public class User_Dto {
 	}
 	public void setU_id(String u_id) {
 		this.u_id = u_id;
+	}
+	public String getU_name() {
+		return u_name;
+	}
+	public void setU_name(String u_name) {
+		this.u_name = u_name;
 	}
 	public String getU_pwd() {
 		return u_pwd;
@@ -41,35 +43,11 @@ public class User_Dto {
 	public void setU_cell(String u_cell) {
 		this.u_cell = u_cell;
 	}
-	public String getU_addr1() {
-		return u_addr1;
+	public String getU_addr() {
+		return u_addr;
 	}
-	public void setU_addr1(String u_addr1) {
-		this.u_addr1 = u_addr1;
-	}
-	public String getU_addr2() {
-		return u_addr2;
-	}
-	public void setU_addr2(String u_addr2) {
-		this.u_addr2 = u_addr2;
-	}
-	public String getU_addr3() {
-		return u_addr3;
-	}
-	public void setU_addr3(String u_addr3) {
-		this.u_addr3 = u_addr3;
-	}
-	public String getU_addr4() {
-		return u_addr4;
-	}
-	public void setU_addr4(String u_addr4) {
-		this.u_addr4 = u_addr4;
-	}
-	public String getU_addr5() {
-		return u_addr5;
-	}
-	public void setU_addr5(String u_addr5) {
-		this.u_addr5 = u_addr5;
+	public void setU_addr(String u_addr) {
+		this.u_addr = u_addr;
 	}
 	public String getU_zipcode() {
 		return u_zipcode;
@@ -77,11 +55,11 @@ public class User_Dto {
 	public void setU_zipcode(String u_zipcode) {
 		this.u_zipcode = u_zipcode;
 	}
-	public int getBirth() {
-		return birth;
+	public int getU_birth() {
+		return u_birth;
 	}
-	public void setBirth(int birth) {
-		this.birth = birth;
+	public void setU_birth(int u_birth) {
+		this.u_birth = u_birth;
 	}
 	public String getU_email() {
 		return u_email;
